ebc0a4ec9ced6aa355b7bd68ab53cd3cd57e804d
remi59800 <remimenart@gmail.com>
2025-05-02 13:58:38 +0200

    fixed height nav

--- src/app/components/BurgerMenu.tsx
+++ src/app/components/BurgerMenu.tsx
@@ -37,7 +37,7 @@ const BurgerMenu: React.FC<BurgerMenuProps> = ({ open, setOpen }) => {
   return (
     <div
       style={{ height: '100svh' }}
-      className={`flex flex-col items-center justify-between bg-green transition-all duration-500 ease-in-out text-center p-8 fixed top-0 right-0 ${
+      className={`flex flex-col items-center justify-between bg-green transition-all duration-500 ease-in-out text-center px-8 py-14 fixed top-0 right-0 ${
         open
           ? 'translate-x-0 opacity-100 scale-100'
           : 'translate-x-full opacity-0 scale-100'
@@ -45,13 +45,12 @@ const BurgerMenu: React.FC<BurgerMenuProps> = ({ open, setOpen }) => {
     >
       <Image
         src='/logo-coq.svg'
-        className='py-14'
         alt='Logo Coq en stock'
         width={65}
         height={52}
       />
 
-      <nav className='flex flex-col max-h-[500px] overflow-y-auto'>
+      <nav className='flex flex-col py-14'>
         <Link
           href='/'
           onClick={handleClose}
@@ -85,35 +84,33 @@ const BurgerMenu: React.FC<BurgerMenuProps> = ({ open, setOpen }) => {
         </Link>
       </nav>
 
-      <div className='py-14'>
-        <div className='flex items-center space-x-6'>
-          <a
-            href='https://www.facebook.com/profile.php?id=100090150523696'
-            aria-label='Facebook'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image
-              src='/icons/Facebook.svg'
-              alt='Facebook'
-              width={35}
-              height={35}
-            />
-          </a>
-          <a
-            href='https://www.instagram.com/coqenstock_marseille/'
-            aria-label='Instagram'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image
-              src='/icons/Instagram.svg'
-              alt='Instagram'
-              width={35}
-              height={35}
-            />
-          </a>
-        </div>
+      <div className='flex items-center space-x-6'>
+        <a
+          href='https://www.facebook.com/profile.php?id=100090150523696'
+          aria-label='Facebook'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <Image
+            src='/icons/Facebook.svg'
+            alt='Facebook'
+            width={35}
+            height={35}
+          />
+        </a>
+        <a
+          href='https://www.instagram.com/coqenstock_marseille/'
+          aria-label='Instagram'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <Image
+            src='/icons/Instagram.svg'
+            alt='Instagram'
+            width={35}
+            height={35}
+          />
+        </a>
       </div>
     </div>
   );
