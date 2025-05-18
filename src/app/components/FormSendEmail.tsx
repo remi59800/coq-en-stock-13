985b386bec65efaa6a497dfc7c9455f84a29c348
remi59800 <remimenart@gmail.com>
2025-05-18 22:30:46 +0200

    page-contact-front-OK

---
+++ src/app/components/FormSendEmail.tsx
@@ -0,0 +1,199 @@
+'use client';
+
+import { useRef, useState } from 'react';
+import Image from 'next/image';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+type FormData = {
+  nom: string;
+  telephone?: string;
+  email: string;
+  message: string;
+};
+
+type PulsingFields = {
+  [key in keyof FormData]?: boolean;
+};
+
+export default function FormSendEmail() {
+  const [formData, setFormData] = useState<FormData>({
+    nom: '',
+    telephone: '',
+    email: '',
+    message: '',
+  });
+
+  const [pulsingFields, setPulsingFields] = useState<PulsingFields>({});
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const validate = () => {
+    const newErrors: Partial<Record<keyof FormData, boolean>> = {};
+    const newPulsing: Partial<Record<keyof FormData, boolean>> = {};
+
+    if (!formData.nom) newErrors.nom = true;
+    if (!formData.email) newErrors.email = true;
+    if (!formData.message) newErrors.message = true;
+
+    // Champs à pulser
+    Object.keys(newErrors).forEach((key) => {
+      newPulsing[key as keyof FormData] = true;
+    });
+
+    setPulsingFields(newPulsing);
+
+    setTimeout(() => {
+      setPulsingFields({});
+    }, 2000);
+
+    return newErrors;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const validationErrors = validate();
+
+    if (Object.keys(validationErrors).length > 0) {
+      toast.warn(
+        "Tous les champs marqués d'une astérisque (*) sont obligatoires."
+      );
+      return;
+    }
+
+    const token = await recaptchaRef.current?.executeAsync();
+    recaptchaRef.current?.reset();
+
+    if (!token) {
+      toast.error('Veuillez valider le reCAPTCHA.');
+      return;
+    }
+
+    console.log('Token reCAPTCHA :', token);
+    console.log('Formulaire prêt à être envoyé :', formData);
+
+    toast.success('Votre message a bien été envoyé ✅');
+
+    setFormData({ nom: '', telephone: '', email: '', message: '' });
+  };
+
+  const inputBase =
+    'border border-primary p-2 bg-[#343333] text-white font-nunito rounded-none focus:outline-none focus:ring-0 placeholder-gray-300';
+  const getInputClass = (field: keyof FormData) =>
+    `${inputBase} ${pulsingFields[field] ? 'animate-pulse' : ''}`;
+
+  return (
+    <>
+      <ToastContainer position='top-right' />
+      <form
+        onSubmit={handleSubmit}
+        className='w-full p-8 bg-green mx-auto flex flex-col gap-4 font-poppins text-sm'
+      >
+        <div className='flex flex-col w-full max-w-[400px] mx-auto'>
+          <h2 className='text-center pb-4 font-black text-lg tracking-[3px]'>
+            ENVOYER UN E-MAIL
+          </h2>
+
+          <label htmlFor='nom' className='font-nunito'>
+            Nom et prénom*
+          </label>
+          <input
+            type='text'
+            name='nom'
+            id='nom'
+            value={formData.nom}
+            onChange={handleChange}
+            maxLength={50}
+            placeholder='Entrez votre nom et prénom'
+            className={getInputClass('nom')}
+          />
+        </div>
+
+        <div className='flex flex-col w-full max-w-[400px] mx-auto'>
+          <label htmlFor='email' className='font-nunito'>
+            Email*
+          </label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            autoComplete='email'
+            value={formData.email}
+            onChange={handleChange}
+            maxLength={100}
+            placeholder='nomprenom@gmail.com'
+            className={getInputClass('email')}
+          />
+        </div>
+
+        <div className='flex flex-col w-full max-w-[400px] mx-auto'>
+          <label htmlFor='telephone' className='font-nunito'>
+            Téléphone
+          </label>
+          <input
+            type='text'
+            name='telephone'
+            id='telephone'
+            value={formData.telephone}
+            onChange={handleChange}
+            maxLength={100}
+            placeholder='Entrez votre numéro'
+            className={getInputClass('email')}
+          />
+        </div>
+
+        <div className='flex flex-col w-full max-w-[400px] mx-auto'>
+          <label htmlFor='message' className='font-nunito'>
+            Message*
+          </label>
+          <textarea
+            name='message'
+            id='message'
+            value={formData.message}
+            onChange={handleChange}
+            rows={5}
+            maxLength={1000}
+            placeholder='Écrivez votre message ici'
+            className={getInputClass('message')}
+          />
+        </div>
+
+        <ReCAPTCHA
+          ref={recaptchaRef}
+          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ''}
+          size='invisible'
+        />
+
+        <div className='flex items-center gap-4 max-w-[400px] mx-auto text-xs'>
+          <input type='checkbox' id='consentement' required />
+          <label htmlFor='consentement' className='font-nunito'>
+            En soumettant ce formulaire, j&apos;accepte que mes données soient
+            utilisées pour me recontacter.
+          </label>
+        </div>
+
+        <button
+          type='submit'
+          className='mt-2 mb-2 font-nunito text-sm border border-primary px-4 py-2 w-full max-w-[400px] mx-auto'
+        >
+          <div className='flex flex-row items-center justify-center'>
+            <Image
+              src='/icons/Send.svg'
+              alt='icon-poulet'
+              width={15}
+              height={15}
+              priority
+            />
+            <p className='pl-2.5'>ENVOYER</p>
+          </div>
+        </button>
+      </form>
+    </>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,73 @@
+import FormSendEmail from '../components/FormSendEmail';
+import Map from '../components/Map';
+
 export default function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
+      <div className='flex flex-col items-center text-primary max-w-3xl mx-auto px-8 pt-3'>
+        <h1 className='text-center pb-4 font-black text-lg tracking-[3px]'>
+          NOUS CONTACTER
+        </h1>
+        <p className='text-center text-sm font-nunito pb-2'>
+          Contactez-nous pour vos commandes ou toute demande de devis pour vos
+          événements traiteurs par téléphone au{' '}
+          <a href='tel:0491911840' className='font-bold'>
+            04 91 91 18 40
+          </a>
+          .
+        </p>
+        <p className='text-center text-sm font-nunito pb-2'>
+          Vous pouvez également nous écrire via le formulaire ci-dessous ou à
+          l’adresse{' '}
+          <a href='mailto:coqenstock13@gmail.com' className='font-bold'>
+            coqenstock13@gmail.com
+          </a>{' '}
+          pour toute question ou demande de devis. Nous vous répondrons
+          rapidement avec toutes les informations nécessaires.
+        </p>
+        <p className='text-center text-sm font-nunito pb-8'>
+          Et bien sûr, n’hésitez pas à venir nous rendre visite sur place au
+          <strong> 95 rue de la République – 13002 Marseille</strong>, nous vous
+          recevrons avec plaisir !
+        </p>
+      </div>
+
+      <div className='flex flex-col mm:flex-row text-primary max-w-5xl mx-auto pb-8'>
+        <div className='w-full mm:w-1/2'>
+          <FormSendEmail />
+        </div>
+        <div className='w-full mm:w-1/2'>
+          <Map />
+        </div>
+      </div>
+
+      <div className='flex flex-col items-center text-primary max-w-3xl mx-auto px-8'>
+        <h2 className='text-center pb-4 font-black text-lg tracking-[3px]'>
+          POINT DE REPÈRES À PROXIMITÉ
+        </h2>
+        <p className='text-center text-sm font-nunito pb-5'>
+          Station de métro le plus proche : <strong> Joliette</strong> <br></br>
+          Stations de tramway les plus proches :
+          <strong> République dames - Joliette</strong>
+          <br></br>
+          Stations de vélo les plus proche :
+          <strong> République moisson - Dames-Schumann</strong>
+          <br></br>
+          Parkings les plus proches :
+          <strong> Q-Park Joliette - Q-Park Phocéens</strong>
+        </p>
+        <p className='text-[0.75rem] leading-normal font-nunito text-center mx-auto mb-9'>
+          Ce site est protégé par reCAPTCHA et la{' '}
+          <a href='https://policies.google.com/privacy' className='underline'>
+            Politique de confidentialité
+          </a>{' '}
+          et les{' '}
+          <a href='https://policies.google.com/terms' className='underline'>
+            Conditions d&apos;utilisation
+          </a>{' '}
+          de Google s’appliquent.
+        </p>
+      </div>
     </div>
   );
 }
